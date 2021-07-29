0e8beb48dd4076dd1bc17644cbf25adb8076c6a6
Creed <ayodeleayoolaoke4@gmail.com>
2021-07-29 08:45:00 +0100

    project pushed to git

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+// $(document).ready(function(){
+//     window.setTimeout('fadeout();',2000);
+// });
+
+// function fadeout(){
+//     $('#loader').fadeOut('slow', function(){
+//         $('#notLoaded').removeClass('notLoaded');
+//     });
+// }
+
+
+// function show(){
+//     document.getElementById(loader);
+//     setTimeout('hide()',3000);
+//     document.getElementById('nav').style.position = 'relative';
+//     document.getElementById('nav').style.display = 'none';
+// }
+// function hide() {
+//     document.getElementById('loader').style.display = 'none';
+//     document.getElementById('nav').style.position = 'fixed';
+//     document.getElementById('nav').style.display = 'block';
+// }
+
+
+
+ // var slide = document.getElementById('slide');
+      // var upArrow = document.getElementById('upArrow');
+      // var downArrow = document.getElementById('downArrow');
+
+      // let x = 0;
+
+      // upArrow.onclick = function(){
+      //     if(x > "-900") {
+      //     x = x - 300;
+      //     slide.style.top = x + "px";
+      //     }
+      // }
+      // downArrow.onclick = function(){
+      //     if(x < 0) {
+      //     x = x + 300;
+      //     slide.style.top = x + "px";
+      //     }
+      // }
+
+// single products
+        
+// const btn__ = document.querySelector("button");
+//       const post = document.querySelector(".post");
+//       const widget = document.querySelector(".star-widget");
+//       const editBtn = document.querySelector(".edit");
+//       btn__.onclick = () => {
+//         widget.style.display = "none";
+//         post.style.display = "block";
+//         return false;
+//       }
+
+
+
+
+
